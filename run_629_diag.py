52085f84f058cefd09584637a93c688eda313faf
trevorcaii <111313569+trevorcaii@users.noreply.github.com>
2024-07-15 11:07:24 -0500

    Update run_629_diag.py

--- run_629_diag.py
+++ run_629_diag.py
@@ -4,7 +4,10 @@ import subprocess
 def main():
     functions.run_command("sudo modprobe -r nvidia_uvm")
     functions.run_command("sudo modprobe -r nvidia_drm")
-    diag_process = subprocess.run(['./fieldiag'], cwd="/home/dell/Downloads/629-INT16-UNIV-ALL")
+    diag_process = subprocess.Popen(['./fieldiag'], cwd="/home/dell/Downloads/629-INT16-UNIV-ALL", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = diag_process.communicate()
+    with open("./629_diag_output.txt", "w") as file:
+        file.write(stdout.decode("utf-8"))
 
 if __name__ == "__main__":
     main()
