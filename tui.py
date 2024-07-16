92aa7125e8e9b1f6804c44aef70523fc9016020c
trevorcaii <111313569+trevorcaii@users.noreply.github.com>
2024-07-16 16:30:40 -0500

    Update tui.py

--- tui.py
+++ tui.py
@@ -337,8 +337,8 @@ def main(stdscr):
             # summary_window.addstr(summary_line_pos, 0, f"Slot Test Counts: {slot_test_counts}")
             # summary_line_pos += int(len(f"Slot Test Counts: {slot_test_counts}")/summary_window_width)
 
-            summary_line_pos = print_with_rollover(f"Tested BDFs: {tested_bdfs}", summary_line_pos)
-            summary_line_pos = print_with_rollover(f"Downstream BDFs: {downstream_bdfs}", summary_line_pos)
+            summary_line_pos = print_with_rollover(f"Tested BDFs:\t{tested_bdfs}", summary_line_pos)
+            summary_line_pos = print_with_rollover(f"Downstream BDFs:\t{downstream_bdfs}", summary_line_pos)
             summary_line_pos = print_with_rollover(f"Slot Numbers: {slot_numbers}", summary_line_pos)
             summary_line_pos = print_with_rollover(f"Slot Test Counts: {slot_test_counts}", summary_line_pos)
 
