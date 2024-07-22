2d735a31366f6921299b69f325238c833e778200
trevorcaii <111313569+trevorcaii@users.noreply.github.com>
2024-07-22 10:32:50 -0500

    Update gpu_burn_script.py

--- gpu_burn_script.py
+++ gpu_burn_script.py
@@ -100,12 +100,14 @@ def gpu_traverse_up():
         pass
 
     if len(gpu_bdf_list) == 0 or  gpu_bdf_list[0] == ' ':
-        gpu_bdf_list = []
-        for bdf in all_bdf_list:
-            class_code_hex = functions.read_class_clode(bdf)
-            header_type = functions.get_header_type(bdf)
-            if class_code_hex[:2] == "03" and header_type == "00": gpu_bdf_list.append(bdf)
+        gpu_bdf_list = functions.identify_gpus()
+        # for bdf in all_bdf_list:
+        #     class_code_hex = functions.read_class_code(bdf)
+        #     header_type = functions.get_header_type(bdf)
+        #     if class_code_hex[:2] == "03" and header_type == "00": gpu_bdf_list.append(bdf)
 
+    print(gpu_bdf_list)
+    time.sleep(10000000)
 
     #get a list of all bdfs with header type 1
     header_bdf_list = [bdf for bdf in all_bdf_list if functions.get_header_type(bdf).startswith("01")]
